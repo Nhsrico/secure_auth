f6d088f7bbe4712911277806bb04095bdb7b4263
Fly Dev <dev@fly.io>
2025-07-07 09:54:28 +0000

    add 2FA verification route

--- lib/secure_auth_web/router.ex
+++ lib/secure_auth_web/router.ex
@@ -68,6 +68,7 @@ defmodule SecureAuthWeb.Router do
       live "/users/register", UserLive.Registration, :new
       live "/users/log-in", UserLive.Login, :new
       live "/users/log-in/:token", UserLive.Confirmation, :new
+      live "/users/verify-2fa", UserLive.Verify2FA, :new
     end
 
     post "/users/log-in", UserSessionController, :create
