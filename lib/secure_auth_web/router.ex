1d1d3158e98c99944f98117538132c6aa082764a
Fly Dev <dev@fly.io>
2025-07-07 09:45:54 +0000

    add 2FA setup route

--- lib/secure_auth_web/router.ex
+++ lib/secure_auth_web/router.ex
@@ -54,6 +54,7 @@ defmodule SecureAuthWeb.Router do
       on_mount: [{SecureAuthWeb.UserAuth, :require_authenticated}] do
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+      live "/users/two-factor-setup", UserLive.TwoFactorSetup, :new
     end
 
     post "/users/update-password", UserSessionController, :update_password
