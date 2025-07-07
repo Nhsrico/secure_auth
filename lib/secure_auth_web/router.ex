1b22b8366b29f652007495df8844e75ab742142b
Fly Dev <dev@fly.io>
2025-07-07 11:05:50 +0000

    add ueberauth plug to router

--- lib/secure_auth_web/router.ex
+++ lib/secure_auth_web/router.ex
@@ -11,6 +11,7 @@ defmodule SecureAuthWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :fetch_current_scope_for_user
+    plug Ueberauth
   end
 
   pipeline :api do
