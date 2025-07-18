50888315cf92955d2d5e87360d6cd6fb287f21be
Fly Dev <dev@fly.io>
2025-07-18 07:09:26 +0000

    add local email adapter for testing

--- config/runtime.exs
+++ config/runtime.exs
@@ -14,8 +14,10 @@ import Config
 #
 #     PHX_SERVER=true bin/secure_auth start
 #
-# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
-# script that automatically sets the env var above.
+# Alternatively, you can use `mix phx.server` or set the RELEASE_NODE
+# environment variable to "remote" to start a remote shell.
+
+# Start the phoenix server if environment variable is set
 if System.get_env("PHX_SERVER") do
   config :secure_auth, SecureAuthWeb.Endpoint, server: true
 end
@@ -31,6 +33,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :secure_auth, SecureAuth.Repo,
+    # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
@@ -50,65 +53,35 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :secure_auth, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
-
   config :secure_auth, SecureAuthWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      # for details about using IPv6 vs IPv4 and loopbacks vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
 
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :secure_auth, SecureAuthWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :secure_auth, SecureAuthWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
+  # Configure the mailer
+  sendgrid_api_key = System.get_env("SENDGRID_API_KEY")
+  mailer_adapter = System.get_env("MAILER_ADAPTER")
 
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  #
-  config :secure_auth, SecureAuth.Mailer,
-    adapter: Swoosh.Adapters.Sendgrid,
-    api_key: System.get_env("SENDGRID_API_KEY")
+  cond do
+    mailer_adapter == "local" ->
+      # Use local adapter for testing - logs emails instead of sending them
+      config :secure_auth, SecureAuth.Mailer, adapter: Swoosh.Adapters.Local
 
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney, Req and Finch out of the box:
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+    is_binary(sendgrid_api_key) and sendgrid_api_key != "" ->
+      # Use SendGrid when API key is available
+      config :secure_auth, SecureAuth.Mailer,
+        adapter: Swoosh.Adapters.Sendgrid,
+        api_key: sendgrid_api_key
+
+    true ->
+      # Fallback to local adapter if no proper configuration
+      config :secure_auth, SecureAuth.Mailer, adapter: Swoosh.Adapters.Local
+  end
 end
